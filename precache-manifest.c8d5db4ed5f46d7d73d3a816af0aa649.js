d7527647335377c1b26223b4161d68888df1c8bd
alex-fuks <alex5825411@gmail.com>
2020-03-26 07:36:49 +0000

    Updates

---
+++ precache-manifest.c8d5db4ed5f46d7d73d3a816af0aa649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfd7e7069dacd0e94d85f743805c57f",
+    "url": "/react-admin-dashboard%20/index.html"
+  },
+  {
+    "revision": "3ddacfb98e2e73d12f2f",
+    "url": "/react-admin-dashboard%20/static/js/2.3e30de39.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/react-admin-dashboard%20/static/js/2.3e30de39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fb28a764c01c3dbdf2",
+    "url": "/react-admin-dashboard%20/static/js/main.c1c94224.chunk.js"
+  },
+  {
+    "revision": "779672fe455eecb150c9",
+    "url": "/react-admin-dashboard%20/static/js/runtime-main.a69ba9db.js"
+  }
+]);
